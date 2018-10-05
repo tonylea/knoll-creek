0ca1e7319871cf43bb7c7e94f98ba83f857398a0
Tony Lea <tonylea@deadrobot.co.uk>
2018-10-05 09:30:13 +0100

    Comment out breaking chnages on server models

--- packages/server/src/models/index.js
+++ packages/server/src/models/index.js
@@ -2,38 +2,33 @@ import Sequelize from 'sequelize';
 
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_INSTANCE,
-    dialect: process.env.DB_DIALECT,
-    define: {
-      underscored: true
-    }
-  }
-);
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_INSTANCE,
+  dialect: process.env.DB_DIALECT,
+  define: {
+    underscored: true,
+  },
+});
 
 const models = {
-  Clusters: sequelize.import('./clusters'),
+  // Clusters: sequelize.import('./clusters'),
   Company: sequelize.import('./company'),
-  CommonIssues: sequelize.import('./commonIssues'),
-  CompanyDirectContacts: sequelize.import('./companyDirectContacts'),
-  DatabaseDetails: sequelize.import('./catabaseDetails'),
+  // CommonIssues: sequelize.import('./commonIssues'),
+  // CompanyDirectContacts: sequelize.import('./companyDirectContacts'),
+  // DatabaseDetails: sequelize.import('./databaseDetails'),
   DayOfWeek: sequelize.import('./dayOfWeek'),
-  ExternalSupportHours: sequelize.import('./externalSupportHours'),
-  InternalSupportHours: sequelize.import('./internalSupportHours'),
-  IPAddress: sequelize.import('./ipAddress'),
+  // ExternalSupportHours: sequelize.import('./externalSupportHours'),
+  // InternalSupportHours: sequelize.import('./internalSupportHours'),
+  // IPAddress: sequelize.import('./ipAddress'),
   Location: sequelize.import('./location'),
   OperatingSystem: sequelize.import('./operatingSystem'),
-  RequiredServices: sequelize.import('./requiredServices'),
-  Servers: sequelize.import('./servers'),
-  SqlInstance: sequelize.import('./sqlInstance'),
+  // RequiredServices: sequelize.import('./requiredServices'),
+  // Servers: sequelize.import('./servers'),
+  // SqlInstance: sequelize.import('./sqlInstance'),
   Status: sequelize.import('./status'),
-  SystemAlias: sequelize.import('./systemAlias'),
-  SystemDependencies: sequelize.import('./systemDependencies'),
-  SystemDetails: sequelize.import('./systemDetails')
+  // SystemAlias: sequelize.import('./systemAlias'),
+  // SystemDependencies: sequelize.import('./systemDependencies'),
+  // SystemDetails: sequelize.import('./systemDetails')
 };
 
 Object.keys(models).forEach(modelName => {
