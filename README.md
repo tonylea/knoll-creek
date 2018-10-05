6dd400385ae5c7437cfedfe786f92790dab9f7ca
Tony Lea <tonylea@nhs.net>
2018-10-05 08:47:39 +0100

    update to yarn workspace setup

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
-# Knoll Creek Server
+# Knoll Creek
 
-As the database schema is worked out, it appears Knoll Creek will perform the following functions:
-
-- Systems Directory
-- Supplier Contact Address Book
-- Server CMDB
-- IP Address List
+This is an application to catalog the various systems an organisation has and their dependencies.
---
+++ packages/client/src/App.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import App from './App';
+
+it('renders without crashing', () => {
+  shallow(<App />);
+});
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
